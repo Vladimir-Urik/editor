f632d8058d73f058fb2f15149edeaa951dc014f7
Vladimir-Urik <gggedrvideos@gmail.com>
2022-08-22 11:54:30 +0200

    Remove demo files

--- composables/states.ts
+++ composables/states.ts
@@ -1,31 +1,6 @@
-import type {File} from './file';
+import type { File } from './file';
 
-export const useFiles = () => useState('files', () => [
-    {
-        'name': 'Home.tsx',
-        'id': '325631751012',
-        'type': 'typescript',
-        'path': 'local/Home.tsx'
-    },
-    {
-        'name': 'Navbar.tsx',
-        'id': '325631156312',
-        'type': 'typescript',
-        'path': 'local/Navbar.tsx'
-    },
-    {
-        'name': 'Sidebar.tsx',
-        'id': '325631531012',
-        'type': 'typescript',
-        'path': 'local/Sidebar.tsx'
-    },
-    {
-        'name': 'Joke.tsx',
-        'id': '325631276012',
-        'type': 'typescript',
-        'path': 'local/Joke.tsx'
-    }
-])
+export const useFiles = () => useState<File[]>('files', () => [])
 
 export const useModals = () => useState('modal', () => {
     return {
